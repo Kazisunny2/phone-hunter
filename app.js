733afc40906168e5e03ddede86230f5a99e7874d
Kazisunny2 <saidul.bit@gmail.com>
2022-03-01 16:41:06 +0100

    Fifth Commit

--- app.js
+++ app.js
@@ -13,14 +13,13 @@ const showPhoneDetails = (phones) => {
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = ` <div>
-    <div class="card border grid p-2 py-5 m-3">
-    <div class="pro-pic">
+    <div class="card border p-2 py-3 m-3">
+    <div>
         <img class="w-50" src="${phone.image}" alt="">
     </div>
-    <h2>Name: ${phone.phone_name}</h2>
+    <h4>Name: ${phone.phone_name}</h2>
     <h5>Brand: ${phone.brand} </h5>
     <div class="allbutton">
-        <button class="btn btn-danger">Others</button>
         <button onclick="details('${phone.slug}')" class="btn btn-success">Details</button>
     </div>
 </div>
@@ -32,5 +31,18 @@ const details = (info) => {
    const url = `https://openapi.programming-hero.com/api/phone/${info}`
    fetch(url)
    .then((response) => response.json())
-   .then((data) => showPhoneDetails(data));
+   .then((data) => setDetails(data.data));
 } 
+const setDetails = (info) => {
+    document.getElementById("details-container").innerHTML =`
+    <div>
+        <h2>Phone Details</h2>
+        <img src="${info.image}" alt="">
+        <h5>Name:${info.name}</h5>
+        <h5>Release Date:${info.releaseDate}</h5>
+        <h5>Cheapset:${info.mainFeatures.chipSet}</h5>
+        <h5>Memory:${info.mainFeatures.memory}</h5>
+        <h5>Storage:${info.mainFeatures.storage}</h5>
+        <h5>Display Size:${info.mainFeatures.displaySize}</h5>
+    </div>`
+}
