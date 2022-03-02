4252b1d5f8579ca57379c42f4688c66243c3c247
Kazisunny2 <saidul.bit@gmail.com>
2022-03-02 05:22:34 +0100

    final commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const allPhones = () =>{ 
     document.getElementById("phone-container").innerHTML= "";
     document.getElementById("error-message").innerHTML= "";
+    document.getElementById("details-container").innerHTML= "";
     const searchValue = document.getElementById("search-box").value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     //console.log(url);
@@ -11,7 +12,7 @@ const allPhones = () =>{
             document.getElementById("error-message").innerHTML=`<h3>No Phone Found</h3>`  
         }
         else{
-            showPhoneDetails(data.data);
+            showPhoneDetails(data.data.slice(0, 20));
         }
     }) 
 }
@@ -21,12 +22,12 @@ const showPhoneDetails = (phones) => {
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = ` <div>
-    <div class="card border p-1 m-3">
+    <div class="card border border-1 p-2 ">
     <div>
         <img class="w-50" src="${phone.image}" alt="">
     </div>
-    <h4>Name: ${phone.phone_name}</h2>
-    <h5>Brand: ${phone.brand} </h5>
+    <h4>Name: ${phone.phone_name}</h4>
+    <h6>Brand: ${phone.brand} </h6>
     <div class="allbutton">
         <button onclick="details('${phone.slug}')" class="btn btn-success">Details</button>
     </div>
@@ -39,18 +40,29 @@ const details = (info) => {
    const url = `https://openapi.programming-hero.com/api/phone/${info}`
    fetch(url)
    .then((response) => response.json())
-   .then((data) => setDetails(data.data));
+    .then((data) => setDetails(data.data)) 
 } 
 const setDetails = (info) => {
     document.getElementById("details-container").innerHTML =`
     <div>
         <h2>Phone Details</h2>
         <img src="${info.image}" alt="">
-        <h5>Name:${info.name}</h5>
-        <h5>Release Date:${info.releaseDate}</h5>
-        <h5>Cheapset:${info.mainFeatures.chipSet}</h5>
-        <h5>Memory:${info.mainFeatures.memory}</h5>
-        <h5>Storage:${info.mainFeatures.storage}</h5>
-        <h5>Display Size:${info.mainFeatures.displaySize}</h5>
+        <h3 class"fw-bold">MainFeatures</h3>
+        <h5>Name: ${info.name}</h5>
+        <h5>Release Date: ${info.releaseDate||'Info will provide soon'}</h5>
+        <h5>Cheapset: ${info.mainFeatures.chipSet||'Info will provide soon'}</h5>
+        <h5>Memory: ${info.mainFeatures.memory||'Info will provide soon'}</h5>
+        <h5>Storage: ${info.mainFeatures.storage||'Info will provide soon'}</h5>
+        <h5>Display Size: ${info.mainFeatures.displaySize||'Info will provide soon'}</h5>
+        <h3 class"fw-bold">Sensor Information</h3>
+        <h5>Sensors: ${info.mainFeatures.sensors||'Info will provide soon'}</h5>
+        <h3 class"fw-bold">Others Information</h3>
+        <h5>Wlan: ${info?.others?.WLAN||'Info will provide soon'}</h5>
+        <h5>Bluetooth": ${info?.others?.Bluetooth||'Info will provide soon'}</h5>
+        <h5>GPS: ${info?.others?.GPS||'Info will provide soon'}</h5>
+        <h5>NFC: ${info?.others?.NFC||'Info will provide soon'}</h5>
+        <h5>Radio: ${info?.others?.Radio||'Info will provide soon'}</h5>
+        <h5>USB: ${info?.others?.USB||'Info will provide soon'}</h5>
+          
     </div>`
 }
